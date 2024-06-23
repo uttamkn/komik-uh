609c08482c96f83a6cd3365c7018873d11aa119d
uttamkn <uttamkn15@gmail.com>
2024-06-23 17:15:34 +0530

    Add a custom font family and define primary and secondary colors

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,11 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#CC313D",
+        secondary: "#F7C5CC",
+        tertiary: "#FF6347",
+      },
+      fontFamily: {
+        heading: ["Bricolage Grotesque"],
+      },
+    },
   },
   plugins: [],
-}
+};
