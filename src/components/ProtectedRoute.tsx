54e648ce927053605e3df528dd613530bea94284
uttamkn <uttamkn15@gmail.com>
2024-06-26 20:46:49 +0530

    Update ProtectedRoute component to correctly pass props to the rendered element

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,24 +1,15 @@
-import { Route, Navigate } from "react-router-dom";
+import { Navigate } from "react-router-dom";
 import { useAuth } from "../context/UserContext";
 import toast from "react-hot-toast";
 
 const ProtectedRoute = ({ component: Component, ...rest }: any) => {
   const { user, loading }: { user: any; loading: boolean } = useAuth();
 
-  return (
-    <Route
-      {...rest}
-      render={(props: any) =>
-        loading ? (
-          toast.loading("Waiting...")
-        ) : user ? (
-          <Component {...props} />
-        ) : (
-          <Navigate to="/" />
-        )
-      }
-    />
-  );
+  if (loading) {
+    toast.loading("Loading...");
+  }
+
+  return user ? <Component {...rest} /> : <Navigate to="/" />;
 };
 
 export default ProtectedRoute;
