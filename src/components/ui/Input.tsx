b75dc8d969ab0070c63f929db36f6d187b0d09d3
uttamkn <uttamkn15@gmail.com>
2024-06-28 13:13:38 +0530

    Update Input component to support search functionality

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -1,20 +1,33 @@
 type InputProps = {
-  label: string;
+  label?: string;
   type: string;
-  value: string;
-  name: string;
+  value?: string;
+  name?: string;
   placeholder?: string;
   pattern?: string;
   required?: boolean | false;
+  search?: boolean | false;
   onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-const Input: React.FC<InputProps> = ({ label, ...props }) => {
+const Input: React.FC<InputProps> = ({ label, search = false, ...props }) => {
+  // If the search prop is true, render a search input
+  if (search) {
+    return (
+      <div>
+        <input
+          className="block p-4 rounded w-full mt-1 border border-black focus:outline-none focus:border-2"
+          {...props}
+        />
+      </div>
+    );
+  }
+
   return (
     <div>
       <label>{label}</label>
       <input
-        className="block px-1.5 py-1 rounded w-full mt-1 border border-black focus:outline-none focus:border-2"
+        className="block p-2 rounded w-full mt-1 border border-black focus:outline-none focus:border-2"
         {...props}
       />
     </div>
