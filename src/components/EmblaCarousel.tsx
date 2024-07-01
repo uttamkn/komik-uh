76520429a3b4048a479c5ce181b2dc1c36f021c5
uttamkn <uttamkn15@gmail.com>
2024-07-01 22:22:50 +0530

    Update EmblaCarousel component to use full height images

--- src/components/EmblaCarousel.tsx
+++ src/components/EmblaCarousel.tsx
@@ -11,13 +11,25 @@ export function EmblaCarousel() {
     <div className="overflow-hidden w-full h-full" ref={emblaRef}>
       <div className="flex">
         <div className="flex-none min-w-0 w-full">
-          <img className="w-full" src={carouselImg1} alt="carouselImg1" />
+          <img
+            className="w-full h-full"
+            src={carouselImg1}
+            alt="carouselImg1"
+          />
         </div>
         <div className="flex-none min-w-0 w-full">
-          <img src={carouselImg2} alt="carouselImg2" />
+          <img
+            className="w-full h-full"
+            src={carouselImg2}
+            alt="carouselImg2"
+          />
         </div>
         <div className="flex-none min-w-0 w-full">
-          <img src={carouselImg3} alt="carouselImg3" />
+          <img
+            className="w-full h-full"
+            src={carouselImg3}
+            alt="carouselImg3"
+          />
         </div>
       </div>
     </div>
